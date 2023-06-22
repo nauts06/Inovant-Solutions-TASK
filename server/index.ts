7f8b48a2613a92396cf98374e655bd5cb7488eb9
Rudra Tiwari <nautscode@gmail.com>
2023-06-22 11:06:05 +0530

    vercel.json

--- server/index.ts
+++ server/index.ts
@@ -14,41 +14,19 @@ dbConnectionTs();
 app.use(express.json());
 // app.use(cors());
 
-
 app.use(express.urlencoded({ extended: true }));
 //routes
 app.use("/api", routes);
 
 // Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 //routes
-app.use('/api',routes)
 
-app.get('/*', (req:any, res:any) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-
-
-
-
-
-app.listen(process.env.PORT || 4000,()=>{
-  console.log("server is running on ", process.env.PORT || 4000);
-})
-
-
-
-
-
-
-
-
-app.get("/*", (req: any, res: any) => {
+app.get("/*", (req: any, res: any) => { 
   res.sendFile(path.join(__dirname, "public", "index.html"));
- 
 });
 
-app.listen(process.env.PORT || 9000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server is running on ", process.env.PORT);
 });
+ 
